6130b5e60846e269e90725348928586fc196bacf
Olivier Lalonde <olalonde@gmail.com>
2011-03-30 20:24:30 -0400

    fixed async bug with client

--- ttychat.js
+++ ttychat.js
@@ -13,18 +13,19 @@ var socket = new net.Socket({type: 'unix'});
 socket.connect(socket_path, function() {
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
+  
+  socket.on('data', function(data) {
+    process.stdin.write(data);
+  });
+
+  socket.on('error', function(data) {
+    process.stdin.write("Server is offline.");
+  });
+
+  socket.on('end', function(data) {
+    process.stdin.write("Disconnected from server.");
+    process.exit();
+  });
+  
   process.stdin.pipe(socket);
-});
-
-socket.on('data', function(data) {
-  process.stdin.write(data);
-});
-
-socket.on('error', function(data) {
-  process.stdin.write("Server is offline.");
-});
-
-socket.on('end', function(data) {
-  process.stdin.write("Disconnected from server.");
-  process.exit();
 });
